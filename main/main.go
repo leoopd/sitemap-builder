ef4214b1ea76becef780f8836df1e58270360756
leo <leo.opdenplatz@gmail.com>
2022-11-24 16:02:23 +0100

    added functionality to filter the links that we are looking for and append them to a list

--- main/main.go
+++ main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	link "github.com/leoopd/html-link_parser/util"
 	build "github.com/leoopd/sitemap-builder/util"
@@ -16,13 +17,34 @@ func check(e error) {
 
 func main() {
 
+	var url string = "https://www.google.com"
+	found := make(map[string]bool)
+	var links []string
 	var list link.LinkedList
-	node, err := link.HtmlParser(build.GrabHtml("https://google.com"))
+
+	node, err := link.HtmlParser(build.GrabHtml(url))
 	check(err)
 
 	link.TagParser(node, &list)
 
 	for i := list.Head; i != nil; i = i.Next {
-		fmt.Println(i.Link)
+		if i.Link[0] == 47 {
+			if !found[i.Link] {
+				found[i.Link] = true
+				links = append(links, url+i.Link)
+			}
+		} else {
+			url2 := strings.TrimLeft(url, "https://")
+			slice := strings.TrimLeft(i.Link, "https://")
+
+			if tmp := strings.Split(slice, "/"); tmp[0] == url2 {
+				links = append(links, "https://"+slice)
+			}
+		}
 	}
+
+	for i := 0; i < len(links); i++ {
+		
+	}
+	fmt.Println(links)
 }
