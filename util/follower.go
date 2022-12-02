ce73fcbc8df0298334b575ac6ca67b197d730f05
leo <leo.opdenplatz@gmail.com>
2022-12-02 20:35:36 +0100

    created follower.go to seperate the functions to follow links

---
+++ util/follower.go
@@ -0,0 +1,26 @@
+package sitemapBuilder
+
+import (
+	"fmt"
+
+	link "github.com/leoopd/html-link_parser/util"
+)
+
+func FollowLinks(links []string, found map[string]bool, visited map[string]bool, list link.LinkedList) []string {
+	for i := 0; i < 10; i++ {
+		if !visited[links[i]] {
+			visited[links[i]] = true
+			newLinks, found := GetLinks(links[i])
+			for j := 0; j < len(newLinks); j++ {
+
+				fmt.Println(newLinks[j])
+				if !found[newLinks[j]] {
+					fmt.Println("new")
+					found[newLinks[j]] = true
+					links = append(links, newLinks[j])
+				}
+			}
+		}
+	}
+	return links
+}
--- util/parser.go
+++ util/parser.go
@@ -35,21 +35,4 @@ func GetLinks(url string, list link.LinkedList, found map[string]bool) []string 
 	return links
 }
 
-// func FollowLinks(links []string, found map[string]bool, visited map[string]bool, list link.LinkedList) []string {
-// 	for i := 0; i < 10; i++ {
-// 		if !visited[links[i]] {
-// 			visited[links[i]] = true
-// 			newLinks, found := GetLinks(links[i])
-// 			for j := 0; j < len(newLinks); j++ {
 
-// 				fmt.Println(newLinks[j])
-// 				if !found[newLinks[j]] {
-// 					fmt.Println("new")
-// 					found[newLinks[j]] = true
-// 					links = append(links, newLinks[j])
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return links
-// }
